74600905fd2cb391c10ca9eef913994b7667a2b0
lastulw <159886411+lastulw@users.noreply.github.com>
2024-02-13 16:54:57 +0800

    Delete dogstar directory

--- dogstar/js/main.js
+++
@@ -1,8 +0,0 @@
-ScrollReveal().reveal('#navigation', { delay: 300 });
-ScrollReveal().reveal('#logo', { delay: 300 });
-ScrollReveal().reveal('.promo-content', { delay: 300 });
-ScrollReveal().reveal('#photo', { delay: 300 });
-ScrollReveal().reveal('#tour', { delay: 300 });
-ScrollReveal().reveal('#video', { delay: 300 });
-ScrollReveal().reveal('#join', { delay: 300 });
-ScrollReveal().reveal('footer', { delay: 300 });
